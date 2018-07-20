4f7d70197ed94f814a4fab76777a0cf8d7ce7c5d
ronnamaeffirmo <ronnwiseley@gmail.com>
2018-07-20 19:13:57 +0800

    run eslint fix FHCS-3 #time 1m

--- test/services/permissions.test.js
+++ test/services/permissions.test.js
@@ -1,10 +1,10 @@
-const assert = require('assert');
-const app = require('../../src/app');
+const assert = require('assert')
+const app = require('../../src/app')
 
 describe('\'permissions\' service', () => {
   it('registered the service', () => {
-    const service = app.service('permissions');
+    const service = app.service('permissions')
 
-    assert.ok(service, 'Registered the service');
-  });
-});
+    assert.ok(service, 'Registered the service')
+  })
+})
--- test/services/roles.test.js
+++ test/services/roles.test.js
@@ -1,10 +1,10 @@
-const assert = require('assert');
-const app = require('../../src/app');
+const assert = require('assert')
+const app = require('../../src/app')
 
 describe('\'roles\' service', () => {
   it('registered the service', () => {
-    const service = app.service('roles');
+    const service = app.service('roles')
 
-    assert.ok(service, 'Registered the service');
-  });
-});
+    assert.ok(service, 'Registered the service')
+  })
+})
