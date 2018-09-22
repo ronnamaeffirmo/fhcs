1d70be82a6dbb80c2664b740b845937c3646331d
Marlon John Ynion <marlonjohnynion@gmail.com>
2018-09-23 00:55:24 +0800

    change data table to cards and refactor item actions FHCS-32 #time 1h 30m

---
+++ src/components/ItemDataCard.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Button, Card, Icon, Menu } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+
+const itemDataCard = ({item, actions}) => {
+  const {quantity, code, name, price, description } = item
+  const {removeItem} = actions
+  return (
+    <Card style={{width: '350px'}}>
+      <Card.Content>
+        <div style={{float: 'right'}}>
+          <span style={styles.quantityHeading}>{quantity} pcs</span>
+        </div>
+        <Card.Header>{name}<span style={styles.code}>{code}</span></Card.Header>
+
+        <Card.Meta>₱{price} per unit</Card.Meta>
+        <Card.Description>
+          {description}
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <div>
+          <Button primary style={styles.actionButton}>Add Inventory</Button>
+
+          <div style={{float: 'right'}}>
+            <Menu.Item as={Link} to={`/item/${item._id}`}>
+              <Button color='green' style={styles.actionButton}><Icon
+                name='edit'/>Edit</Button>
+            </Menu.Item>
+            <Button color='red' style={styles.actionButton} onClick={() => removeItem(item._id)}><Icon
+              name='delete'/>Delete</Button>
+          </div>
+        </div>
+      </Card.Content>
+    </Card>
+  )
+}
+
+const styles = {
+  actionButton: {
+    color: '#fff',
+    borderRadius: '0.25em',
+    marginRight: '7px',
+    padding: '0.767em',
+    border: 'none',
+    textShadow: '0 1px 1px rgba(0,0,0,.1)'
+  },
+  quantityHeading: {
+    fontWeight: 'bold',
+    fontSize: '13px'
+  },
+  code: {
+    fontSize: '10px',
+    paddingLeft: '3px',
+    display: 'inline-block',
+    verticalAlign: 'top',
+    lineHeight: 'normal'
+  }
+}
+export default itemDataCard
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react'
-import { Table, Icon, Segment, Button, Menu, Container, Input, Header } from 'semantic-ui-react'
+import {
+  Button,
+  Card,
+  Container,
+  Icon,
+  Input,
+  Menu,
+  Segment,
+  Table
+} from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
-
+import ItemDataCard from './ItemDataCard'
 // code: String,
 // name: String,
 // description: String,
@@ -15,72 +24,23 @@ class ItemList extends Component {
   }
 
   render () {
-    let { items, removeItem } = this.props
+    let {items, removeItem} = this.props
     items = items || []
     return (
       <Container style={styles.mainContainer}>
-        <Input placeholder='Search items here...' style={styles.itemSearchField}/>
+        <Input placeholder='Search items here...'
+               style={styles.itemSearchField}/>
         <Button primary style={styles.searchButton}>Add Item</Button>
         <Container style={styles.itemContainer}>
-          <Header as='h2' style={styles.itemContainer.itemHeadline}>Hollow Block 3 x 2</Header>
-          <p>13.75 per unit -- 7 units left</p>
-          <Container>
-            <Button primary style={styles.itemContainer.actionButton}>Add Inventory</Button>
-            <Button primary style={styles.itemContainer.actionButton}>Edit Item</Button>
-            <Button primary style={styles.itemContainer.actionButton}>Add Sales Record</Button>
-          </Container>
-        </Container>
-
-        <Table celled selectable>
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Status</Table.HeaderCell>
-              <Table.HeaderCell>Code</Table.HeaderCell>
-              <Table.HeaderCell>Name</Table.HeaderCell>
-              <Table.HeaderCell>Description</Table.HeaderCell>
-              <Table.HeaderCell>Price</Table.HeaderCell>
-              <Table.HeaderCell>Quantity</Table.HeaderCell>
-              <Table.HeaderCell>Unit</Table.HeaderCell>
-              <Table.HeaderCell>Action</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-
-          <Table.Body>
-            { items.map((item) => {
-              const error = (!item.quantity || item.quantity === 0) && true
-              const warning = item.quantity < 20 && true
-              const status = error ? 'Unavailable' : (warning && 'Low quantity')
+          <Card.Group centered>
+            {items.map((item) => {
               return (
-                <Table.Row key={item._id} error={error} warning={warning}>
-                  <Table.Cell>{status && <Icon name='attention' />}{status}</Table.Cell>
-                  <Table.Cell>{item.code}</Table.Cell>
-                  <Table.Cell>{item.name}</Table.Cell>
-                  <Table.Cell>{item.description}</Table.Cell>
-                  <Table.Cell>{item.price}</Table.Cell>
-                  <Table.Cell>{item.quantity}</Table.Cell>
-                  <Table.Cell>{item.unit}</Table.Cell>
-                  <Table.Cell collapsing>
-                    <Button.Group>
-                      <Menu.Item as={Link} to={`/item/${item._id}`}>
-                        <Button icon>
-                          <Segment inverted>
-                            <Icon inverted color='green' name='edit' />
-                          </Segment>
-                        </Button>
-                      </Menu.Item>
-                      <Button.Or />
-                      <Button icon onClick={() => removeItem(item._id)}>
-                        <Segment inverted>
-                          <Icon inverted color='red' name='trash alternate' />
-                        </Segment>
-                      </Button>
-                    </Button.Group>
-                  </Table.Cell>
-                </Table.Row>
+                <ItemDataCard item={item} key={item._id} actions={{removeItem}}/>
               )
-            })}
-          </Table.Body>
-        </Table>
+            })
+            }
+          </Card.Group>
+        </Container>
       </Container>
 
     )
@@ -91,32 +51,28 @@ const styles = {
   mainContainer: {
     width: '768px !important',
     padding: '10px',
-    marginTop: '10px',
-    backgroundColor: '#F9F9F9'
+    marginTop: '10px'
   },
   itemSearchField: {
-    width: '77%',
-    marginRight: '10px'
+    width: '77%'
   },
   searchButton: {
-    width: '21%'
+    width: '21%',
+    float: 'right'
   },
   itemContainer: {
-    height: '200px',
     marginTop: '30px',
     marginBottom: '30px',
     marginLeft: '15px',
     marginRight: '15px',
     paddingTop: '7px',
-    paddingLeft: '30px',
-    backgroundColor: 'white',
     itemHeadline: {
       marginTop: '35px'
     },
     actionButton: {
       color: '#fff',
       borderRadius: '0.25em',
-      marginRight: '13px',
+      marginRight: '7px',
       padding: '0.767em',
       border: 'none',
       textShadow: '0 1px 1px rgba(0,0,0,.1)'
