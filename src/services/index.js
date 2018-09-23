9e29c5d2129ec713ec7d8cde7914db7e509e828a
Marlon John Ynion <marlonjohnynion@gmail.com>
2018-09-23 16:36:50 +0800

    add inventories service FHCS-31 #time 10m

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,12 @@ const users = require('./users/users.service.js')
 const items = require('./items/items.service.js')
 const permissions = require('./permissions/permissions.service.js')
 const roles = require('./roles/roles.service.js')
+const inventories = require('./inventories/inventories.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users)
   app.configure(items)
   app.configure(permissions)
   app.configure(roles)
+  app.configure(inventories);
 }
