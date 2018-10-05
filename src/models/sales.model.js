38b6609d999854a0fcb938437e3eb3e02d5bb2ee
unknown <angellou101999@gmail.com>
2018-10-05 14:44:59 +0800

    add sales service FHCS-32 #time 15m

---
+++ src/models/sales.model.js
@@ -0,0 +1,24 @@
+// sales-model.js - A mongoose model
+// 
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+  const sales = new Schema({
+    customer: String,
+    dateFrom: String,
+    discount: Number,
+    grandTotal: Number,
+    invoiceNumber: String,
+    itemLists: [],
+    quantity: String,
+    remarks: String,
+    terms: String,
+  }, {
+    timestamps: true,
+    versionKey: false
+  });
+
+  return mongooseClient.model('sales', sales);
+};
--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,11 @@
 const users = require('./users/users.service.js')
 const item = require('./item/item.service.js')
+const sales = require('./sales/sales.service.js');
+const customers = require('./customers/customers.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users)
   app.configure(item)
+  app.configure(sales);
+  app.configure(customers);
 }
