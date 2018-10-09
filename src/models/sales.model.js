ad2de54ec2f82a6ed445d39062f8e749b5115c01
ronnamaeffirmo <ronnwiseley@gmail.com>
2018-10-09 11:59:58 +0800

    format to standardjs fhcs-32

--- src/models/sales.model.js
+++ src/models/sales.model.js
@@ -1,10 +1,10 @@
 // sales-model.js - A mongoose model
-// 
+//
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
-  const mongooseClient = app.get('mongooseClient');
-  const { Schema } = mongooseClient;
+  const mongooseClient = app.get('mongooseClient')
+  const { Schema } = mongooseClient
   const sales = new Schema({
     customer: String,
     dateFrom: String,
@@ -15,11 +15,11 @@ module.exports = function (app) {
     itemLists: [],
     quantity: String,
     remarks: String,
-    terms: String,
+    terms: String
   }, {
     timestamps: true,
     versionKey: false
-  });
+  })
 
-  return mongooseClient.model('sales', sales);
-};
+  return mongooseClient.model('sales', sales)
+}
