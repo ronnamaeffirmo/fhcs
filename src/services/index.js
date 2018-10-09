febea36e1580b81b8810564b533232b64e4ccd10
Ronna Mae Firmo <ronnwiseley@gmail.com>
2018-10-09 03:34:39 +0000

    fix conflict in services index.js

--- src/services/index.js
+++ src/services/index.js
@@ -2,10 +2,18 @@ const users = require('./users/users.service.js')
 const item = require('./item/item.service.js')
 const sales = require('./sales/sales.service.js');
 const customers = require('./customers/customers.service.js');
-// eslint-disable-next-line no-unused-vars
+const permissions = require('./permissions/permissions.service.js')
+const roles = require('./roles/roles.service.js')
+const inventories = require('./inventories/inventories.service.js');
+
 module.exports = function (app) {
   app.configure(users)
   app.configure(item)
   app.configure(sales);
   app.configure(customers);
+  app.configure(permissions)
+  app.configure(roles)
+  app.configure(inventories);
 }
+
+
