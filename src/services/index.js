14c30b5be65854a48623e7ac2bffdcd7bf127fb8
Ronna Mae Firmo <ronnwiseley@gmail.com>
2018-10-09 03:38:06 +0000

    update fix on /services/index.js

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,5 @@
 const users = require('./users/users.service.js')
-const item = require('./item/item.service.js')
+const items = require('./items/items.service.js')
 const sales = require('./sales/sales.service.js');
 const customers = require('./customers/customers.service.js');
 const permissions = require('./permissions/permissions.service.js')
@@ -8,12 +8,10 @@ const inventories = require('./inventories/inventories.service.js');
 
 module.exports = function (app) {
   app.configure(users)
-  app.configure(item)
+  app.configure(items)
   app.configure(sales);
   app.configure(customers);
   app.configure(permissions)
   app.configure(roles)
   app.configure(inventories);
 }
-
-
