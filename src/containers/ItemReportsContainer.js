4935f7a37c742daa54a80fa13a646867516a2e3a
ronnamaeffirmo <ronnwiseley@gmail.com>
2018-10-09 11:00:44 +0800

    base component for reports fhcs-33

--- src/containers/ItemReportsContainer.js
+++ src/containers/ItemReportsContainer.js
@@ -1,6 +1,11 @@
 import { connect } from 'react-redux'
+import { reduxForm } from 'redux-form'
 
 import ItemReports from '../components/ItemReports'
+
+const wrapped = reduxForm({
+  form: 'period'
+})(ItemReports)
 
 const mapStateToProps = (state) => ({
 })
@@ -11,4 +16,4 @@ const mapDispatchToProps = (dispatch) => ({
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(ItemReports)
+)(wrapped)
