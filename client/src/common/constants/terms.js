cfec86dd76928daaa74f591de79aaf3e8af7b7ae
Marlon John Ynion <marlonjohnynion@gmail.com>
2018-12-02 03:52:39 +0800

    add more net options FHCS-48 #time 5m

--- client/src/common/constants/terms.js
+++ client/src/common/constants/terms.js
@@ -1,5 +1,29 @@
 const netOptions = [
   {
+    key: 'net0',
+    name: 'Net0',
+    value: '0',
+    text: 'Net 0'
+  },
+  {
+    key: 'net1',
+    name: 'Net1',
+    value: '1',
+    text: 'Net 1'
+  },
+  {
+    key: 'net3',
+    name: 'Net3',
+    value: '3',
+    text: 'Net 3'
+  },
+  {
+    key: 'net7',
+    name: 'Net7',
+    value: '7',
+    text: 'Net 7'
+  },
+  {
     key: 'net30',
     name: 'Net30',
     value: '30',
