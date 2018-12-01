ee2dfc7c77d2e7c3de797a2da883e3b1633c3b7c
Marlon John Ynion <marlonjohnynion@gmail.com>
2018-12-01 16:45:19 +0800

    remove functions that were previously used by redux when handling `create sale` FHCS-48 #time 10m #comment functions are handled directly by redux-form state management

--- client/src/reducers/saleReducer.js
+++ client/src/reducers/saleReducer.js
@@ -1,37 +1,12 @@
 import {
-  GET_CUSTOMERS,
-  ON_CUSTOMER_DROPDOWN_CHANGE,
-  ON_INVOICE_NUMBER_CHANGE,
-  ON_CALENDAR_DATE_CHANGE,
-  ON_NET_TERMS_CHANGE,
-  COMPUTE_DATE_TO,
-  ON_REMARKS_CHANGE,
-  ON_ITEM_SEARCH,
-  ON_ITEM_SEARCH_RESULT,
-  ON_SELECT_SEARCH_RESULT,
-  ON_POPULATE,
-  ON_PRICE_CHANGE,
-  ON_DISCOUNT_CHANGE,
-  ON_QUANTITY_CHANGE,
-  BUTTON_ADD_ITEM,
-  CANCEL_BUTTON,
-  REMOVE_FROM_LIST,
-  CREATE_SALES_RECORD,
-  GET_SALES,
-  REMOVE_SALE
+  ADD_SALE,
+  REMOVE_SALE,
+  RECEIVE_SALES,
+  RECEIVE_SALE
 } from '../actions/salesAction'
 
 const initialState = {
   list: [],
-  customer: undefined,
-  invoiceNumber: undefined,
-  dateFrom: undefined,
-  terms: undefined,
-  dateTo: undefined,
-  remarks: undefined,
-  discount: undefined,
-  itemLists: [],
-  sales: []
 }
 
 const saleReducer = (state = initialState, action) => {
@@ -54,120 +29,9 @@ const saleReducer = (state = initialState, action) => {
         sales: action.payload
       }
     }
-    case CREATE_SALES_RECORD: {
-      return {
-        ...state,
-        sales: [...state.sales, action.payload]
-      }
-    }
-    case CANCEL_BUTTON: {
-      return {
-        ...state,
-        customer: action.payload.customer,
-        invoiceNumber: action.payload.invoiceNumber,
-        dateFrom: action.payload.dateFrom,
-        terms: action.payload.terms,
-        dateTo: action.payload.dateTo,
-        remarks: action.payload.remarks,
-        discount: action.payload.discount,
-        itemLists: action.payload.itemLists,
-        searchValue: action.payload.searchValue,
-        price: action.payload.price
-      }
-    }
-    case BUTTON_ADD_ITEM: {
-      return {
-        ...state,
-        itemLists: [...state.itemLists, action.payload]
-      }
-    }
-    case REMOVE_FROM_LIST: {
-      return {
-        ...state,
-        itemLists: state.itemLists.filter(item => item._id !== action.payload)
-      }
-    }
-    case ON_QUANTITY_CHANGE: {
-      return {
-        ...state,
-        quantity: action.payload
-      }
-    }
-    case ON_DISCOUNT_CHANGE: {
-      return {
-        ...state,
-        discount: action.payload
-      }
-    }
-    case ON_PRICE_CHANGE: {
-      return {
-        ...state,
-        price: action.payload
-      }
-    }
-    case ON_SELECT_SEARCH_RESULT: {
-      return {
-        ...state,
-        selectedResult: action.payload
-      }
-    }
-    case ON_POPULATE: {
-      return {
-        ...state,
-        price: action.payload.price,
-        searchValue: action.payload.searchValue
-      }
-    }
-    case ON_ITEM_SEARCH_RESULT: {
-      return {
-        ...state,
-        itemSearchResult: action.payload
-      }
-    }
-    case ON_ITEM_SEARCH: {
-      return {
-        ...state,
-        searchValue: action.payload
-      }
-    }
-    case ON_REMARKS_CHANGE: {
-      return {
-        ...state,
-        remarks: action.payload
-      }
-    }
-    case ON_CUSTOMER_DROPDOWN_CHANGE: {
-      return {
-        ...state,
-        customer: action.payload
-      }
-    }
-    case ON_INVOICE_NUMBER_CHANGE: {
-      return {
-        ...state,
-        invoiceNumber: action.payload
-      }
-    }
-    case ON_CALENDAR_DATE_CHANGE: {
-      return {
-        ...state,
-        dateFrom: action.payload
-      }
-    }
-    case ON_NET_TERMS_CHANGE: {
-      return {
-        ...state,
-        terms: action.payload
-      }
-    }
-    case COMPUTE_DATE_TO: {
-      return {
-        ...state,
-        dateTo: action.payload
-      }
-    }
-    default:
+    default: {
       return state
+    }
   }
 }
 
