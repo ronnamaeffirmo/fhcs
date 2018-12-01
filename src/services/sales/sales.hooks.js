1bee72fd93324e1f354406694b5570dbb8f9d3d2
unknown <angellou101999@gmail.com>
2018-12-01 23:39:07 +0800

    remove permission.test.js FHCS-39 #time 1m

--- src/services/sales/sales.hooks.js
+++ src/services/sales/sales.hooks.js
@@ -1,5 +1,8 @@
+// const { authenticate } = require('@feathersjs/authentication').hooks
+
 module.exports = {
   before: {
+    // all: [ authenticate('jwt') ], //
     all: [],
     find: [],
     get: [],
