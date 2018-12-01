e36603978e2d2a30f61d22892dc0bd9700fb1928
unknown <angellou101999@gmail.com>
2018-12-01 23:37:03 +0800

    fix reducer tests

--- client/src/tests/customerReducer.test.js
+++ client/src/tests/customerReducer.test.js
@@ -3,14 +3,14 @@ import reducer from '../reducers/customerReducer'
 
 describe('customer reducer', () => {
   it('should return the initial state', () => {
-    const initialState = { customers: [] }
+    const initialState = { customers: [], filteredCustomers:  [] }
     expect(reducer(undefined, {})).toEqual(initialState)
   })
 
   it('should handle ADD_CUSTOMER', () => {
     const data = {name: 'customer1'}
     const action = { type: ADD_CUSTOMER, payload: data}
-    const expectedState = { customers: [data] }
+    const expectedState = { customer: {"name": "customer1"}, customers: [{"name": "customer1"}], filteredCustomers: [] }
     
     expect(reducer(undefined, action)).toEqual(expectedState)
   })
--- client/src/tests/inventoryReducer.test.js
+++ client/src/tests/inventoryReducer.test.js
@@ -3,14 +3,14 @@ import reducer from '../reducers/inventoryReducer'
 
 describe('inventory reducer', () => {
   it('should return initial state', () => {
-    const initialState = { inventories: [] }
+    const initialState = { filteredInventories:  [], inventories: [] }
     expect(reducer(undefined, {})).toEqual(initialState)
   })
 
   it('should handle GET_INVENTORIES', () => {
     const datas = [{name: 'door knob', qty: '4', unit: 'pcs'},{name: 'hollow block', qty: '10', unit: 'pcs'}]
     const action = {type: GET_INVENTORIES, payload: datas}
-    const expectedState = { inventories: datas}
+    const expectedState = { filteredInventories:  [], inventories: datas}
     
     expect(reducer(undefined, action)).toEqual(expectedState)
   })
