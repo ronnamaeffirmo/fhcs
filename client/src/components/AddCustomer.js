db2a96381571178fc3e73f7f70792bf2efca5203
ronnamaeffirmo <ronnwiseley@gmail.com>
2018-12-03 22:01:06 +0800

    modify add customer modal to support new fields FHCS-40 #time 10m

--- client/src/components/AddCustomer.js
+++ client/src/components/AddCustomer.js
@@ -9,29 +9,49 @@ const required = value => value ? undefined : 'Required'
 const AddCustomerForm = ({addCustomer, handleSubmit, pristine, submitting}) => (
   <Modal size="tiny" trigger={<Button style={styles.searchButton}>NEW CUSTOMER</Button>}
     centered={false}>
-    <Modal.Header>Add A New Customer</Modal.Header>
+    <Modal.Header>Add a New Customer</Modal.Header>
     <Modal.Content>
       <Form onSubmit={handleSubmit(addCustomer)}>
+        {/*
+          name: { type: 'String', required: true },
+          address: { type: 'String', required: true },
+          phone: { type: 'String', required: true },
+          company: { type: 'String' }
+        */}
         <Field
           type='text'
-          name='firstname'
-          label='First Name'
-          placeholder='First name'
+          name='name'
+          label='Name'
+          icon='user'
+          placeholder='Name'
           component={InputField}
           validate={[required]}
         />
-        <Field
-          type='text'
-          name='lastname'
-          label='Last Name'
-          placeholder='Last name'
-          component={InputField}
-          validate={[required]}
-        />
+        <Form.Group widths='equal'>
+          <Field
+            type='text'
+            name='phone'
+            label='Phone'
+            icon='phone'
+            placeholder='Phone'
+            component={InputField}
+            validate={[required]}
+          />
+          <Field
+            type='text'
+            name='company'
+            label='Company'
+            icon='building outline'
+            placeholder='Company'
+            component={InputField}
+            validate={[required]}
+          />
+        </Form.Group>
         <Field
           type='text'
           name='address'
           label='Address'
+          icon='map marker alternate'
           placeholder='Address'
           component={InputField}
           validate={[required]}
