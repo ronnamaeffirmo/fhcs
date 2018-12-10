d58837e8dd9d98a92f7aa4911bb25735467b4612
ronnamaeffirmo <ronnwiseley@gmail.com>
2018-12-11 05:14:23 +0800

    login hihi FHCS-41 #time 2h

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, Container, Form, Grid, Image } from 'semantic-ui-react'
+import { Button, Container, Form, Grid, Icon, Image } from 'semantic-ui-react'
 import { reduxForm } from 'redux-form'
 import ScrollLock from 'react-scrolllock'
 
@@ -28,32 +28,21 @@ class LoginForm extends React.Component {
   }
 
   render () {
+    const { username, password } = this.state
     const { isAuthenticated, handleLogout } = this.props
+
     return (
       <Grid columns='equal' style={{ margin: 0 }}>
         <Grid.Column style={style.imageColumn} width={12}>
-          <img style={style.coverImage} src='/images/fieldstone.jpg' />
+          <img style={style.coverImage} src='/images/fieldstone-poser.jpg' />
         </Grid.Column>
-        <Grid.Column style={style.form}>
+        <Grid.Column style={style.form} textAlign='center'>
+          <Image centered style={style.logo} src='/images/fieldstone-logo-flat.png' />
+          <div style={style.header}>Login to your account</div>
           <Form className="left column">
-            <Form.Field>
-              <label>Username</label>
-              <Form.Input placeholder='username' name='username' type='username' onChange={(e) => this.handleInput(e)}/>
-            </Form.Field>
-            <Form.Field>
-              <label>Password</label>
-              <Form.Input placeholder='password' name='password' type='password' onChange={(e) => this.handleInput(e)}/>
-            </Form.Field>
-            <Container>
-              {
-                /**
-                 * show that login and logout is working
-                 * sa ibang page dapat ang logout. Like sa Header of something home page :)
-                 */
-                isAuthenticated ? <Button color='teal' onClick={handleLogout} content='Logout'/>
-                  : <Button color='teal' onClick={(e) => this.handleLogin(e)} content='Login'/>
-              }
-            </Container>
+            <Form.Input style={style.inputs} placeholder='Username' icon='mail' name='username' type='username' onChange={(e) => this.handleInput(e)}/>
+            <Form.Input style={style.inputs} placeholder='Password' icon='key' name='password' type='password' onChange={(e) => this.handleInput(e)}/>
+            <Button disabled={!username || !password} style={style.loginBtn} fluid color='teal' onClick={(e) => this.handleLogin(e)} content='Login'/>
           </Form>
         </Grid.Column>
         <ScrollLock />
@@ -67,10 +56,29 @@ const style = {
     height: '100vh', padding: 0
   },
   coverImage: {
-    objectFit: 'cover', width: '100%', height: '100%'
+    // object position specific to fieldstone-poser image
+    objectFit: 'cover', objectPosition: '20% 13px', width: '100%', height: '100%'
   },
   form: {
-    padding: '120px 48px'
+    padding: '130px 48px'
+  },
+  logo: {
+    width: '100px',
+    height: 'auto'
+  },
+  header: {
+    fontSize: '1.4rem',
+    textTransform: 'uppercase',
+    marginBottom: '3.8rem',
+    marginTop: '2.25rem'
+  },
+  inputs: {
+    height: '55px'
+  },
+  loginBtn: {
+    marginTop: '35px',
+    // lol got boxShadow from fuse
+    boxShadow: '0px 1px 5px 0px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12)'
   }
 }
 
