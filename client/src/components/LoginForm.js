9f07713092f1ebaf2cf15c76a5bc2a3e1de7819b
ronnamaeffirmo <ronnwiseley@gmail.com>
2018-12-11 03:18:46 +0800

    login page (with temporary image) FHCS-41 #time 20m

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import { Button, Container, Form } from 'semantic-ui-react'
+import { Button, Container, Form, Grid, Image } from 'semantic-ui-react'
 import { reduxForm } from 'redux-form'
+import ScrollLock from 'react-scrolllock'
+
 
 class LoginForm extends React.Component {
   constructor (props) {
@@ -28,27 +30,47 @@ class LoginForm extends React.Component {
   render () {
     const { isAuthenticated, handleLogout } = this.props
     return (
-      <Form className="left column">
-        <Form.Field>
-          <label>Username</label>
-          <Form.Input placeholder='username' name='username' type='username' onChange={(e) => this.handleInput(e)}/>
-        </Form.Field>
-        <Form.Field>
-          <label>Password</label>
-          <Form.Input placeholder='password' name='password' type='password' onChange={(e) => this.handleInput(e)}/>
-        </Form.Field>
-        <Container>
-          {
-            /**
-             * show that login and logout is working
-             * sa ibang page dapat ang logout. Like sa Header of something home page :)
-             */
-            isAuthenticated ? <Button color='teal' onClick={handleLogout} content='Logout'/>
-              : <Button color='teal' onClick={(e) => this.handleLogin(e)} content='Login'/>
-          }
-        </Container>
-      </Form>
+      <Grid columns='equal' style={{ margin: 0 }}>
+        <Grid.Column style={style.imageColumn} width={12}>
+          <img style={style.coverImage} src='/images/fieldstone.jpg' />
+        </Grid.Column>
+        <Grid.Column style={style.form}>
+          <Form className="left column">
+            <Form.Field>
+              <label>Username</label>
+              <Form.Input placeholder='username' name='username' type='username' onChange={(e) => this.handleInput(e)}/>
+            </Form.Field>
+            <Form.Field>
+              <label>Password</label>
+              <Form.Input placeholder='password' name='password' type='password' onChange={(e) => this.handleInput(e)}/>
+            </Form.Field>
+            <Container>
+              {
+                /**
+                 * show that login and logout is working
+                 * sa ibang page dapat ang logout. Like sa Header of something home page :)
+                 */
+                isAuthenticated ? <Button color='teal' onClick={handleLogout} content='Logout'/>
+                  : <Button color='teal' onClick={(e) => this.handleLogin(e)} content='Login'/>
+              }
+            </Container>
+          </Form>
+        </Grid.Column>
+        <ScrollLock />
+      </Grid>
     )
+  }
+}
+
+const style = {
+  imageColumn: {
+    height: '100vh', padding: 0
+  },
+  coverImage: {
+    objectFit: 'cover', width: '100%', height: '100%'
+  },
+  form: {
+    padding: '120px 48px'
   }
 }
 
