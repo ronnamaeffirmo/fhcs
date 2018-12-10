8e0465b6aaca0e3206d6d83a04645cc0b77c607c
ronnamaeffirmo <ronnwiseley@gmail.com>
2018-12-11 05:17:13 +0800

    change btn bg to match outlines FHCS-41 #time 3m

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -76,6 +76,7 @@ const style = {
     height: '55px'
   },
   loginBtn: {
+    backgroundColor: '#475462',
     marginTop: '35px',
     // lol got boxShadow from fuse
     boxShadow: '0px 1px 5px 0px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 3px 1px -2px rgba(0, 0, 0, 0.12)'
