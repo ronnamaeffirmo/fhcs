d7d499bf61500f71bcd905c88e6f091e8b61fcbd
Marlon John Ynion <marlonjohnynion@gmail.com>
2018-12-12 06:32:21 +0800

    user form bug fixes FHCS-35 #time 10m

--- src/hooks/add-aggregated-fields.js
+++ src/hooks/add-aggregated-fields.js
@@ -1,6 +1,5 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
-const mongoose = require('mongoose')
 
 // eslint-disable-next-line no-unused-vars
 module.exports = function (options = {}) {
