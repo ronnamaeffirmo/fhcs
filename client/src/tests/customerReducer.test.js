d4eef7ef160d3638282fdf796c5f4ab69f070ff9
unknown <angellou101999@gmail.com>
2018-12-15 02:21:47 +0800

    fix src/tests/customerReducer.test.js FHCS-38 #time 10m

--- client/src/tests/customerReducer.test.js
+++ client/src/tests/customerReducer.test.js
@@ -3,14 +3,14 @@ import reducer from '../reducers/customerReducer'
 
 describe('customer reducer', () => {
   it('should return the initial state', () => {
-    const initialState = { customers: [], filteredCustomers:  [] }
+    const initialState = {"customers": [], "filteredList": [], "loading": false}
     expect(reducer(undefined, {})).toEqual(initialState)
   })
 
   it('should handle ADD_CUSTOMER', () => {
     const data = {name: 'customer1'}
     const action = { type: ADD_CUSTOMER, payload: data}
-    const expectedState = { customer: {"name": "customer1"}, customers: [{"name": "customer1"}], filteredCustomers: [] }
+    const expectedState = {"customer": {"name": "customer1"}, "customers": [{"name": "customer1"}], "filteredList":  [], "loading": false}
     
     expect(reducer(undefined, action)).toEqual(expectedState)
   })
