6f73951415be4741c7c962621081c1be620e140b
unknown <angellou101999@gmail.com>
2018-12-15 02:42:25 +0800

    fix test errors frontend FHCS-38 #time 30m

--- client/src/tests/inventoryReducer.test.js
+++ client/src/tests/inventoryReducer.test.js
@@ -3,14 +3,14 @@ import reducer from '../reducers/inventoryReducer'
 
 describe('inventory reducer', () => {
   it('should return initial state', () => {
-    const initialState = { filteredInventories:  [], inventories: [] }
+    const initialState = {"filteredList":  [], "gettingInventory": false, "inventories":  [], "inventory": {}, "loading": false}
     expect(reducer(undefined, {})).toEqual(initialState)
   })
 
   it('should handle GET_INVENTORIES', () => {
     const datas = [{name: 'door knob', qty: '4', unit: 'pcs'},{name: 'hollow block', qty: '10', unit: 'pcs'}]
     const action = {type: GET_INVENTORIES, payload: datas}
-    const expectedState = { filteredInventories:  [], inventories: datas}
+    const expectedState = {"filteredList":  [], "gettingInventory": false, "inventories": [{"name": "door knob", "qty": "4", "unit": "pcs"}, {"name": "hollow block", "qty": "10", "unit": "pcs"}], "inventory": {}, "loading": false}
 
     expect(reducer(undefined, action)).toEqual(expectedState)
   })
