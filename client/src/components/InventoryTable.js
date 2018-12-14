ed5c9b9653d198e25064a896fbd9987ee41e3919
Marlon John Ynion <marlonjohnynion@gmail.com>
2018-12-14 10:40:25 +0800

    resolve merge errors

--- client/src/components/InventoryTable.js
+++ client/src/components/InventoryTable.js
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 import React, { Fragment } from 'react'
-import { Table, Segment, Loader, Message } from 'semantic-ui-react'
-=======
-import React from 'react'
 import { Button, Divider, Input, Message, Segment, Table } from 'semantic-ui-react'
+import Loader from '../components/Loader'
 import _ from 'lodash'
 import moment from 'moment'
->>>>>>> finalization
 
 const styles = {
   mainContainer: {
@@ -20,28 +16,7 @@ const styles = {
 }
 
 const InventoryTable = props => {
-<<<<<<< HEAD
-  const {inventories, loading} = props
-  return (
-    <div style={styles.mainContainer}>
-      { loading 
-        ? <Segment vertical padded>
-            <Loader active />
-          </Segment>
-        : <Fragment>
-          {inventories && !inventories.length && <Message negative>No available inventory yet</Message>}
-          <Table celled sortable>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell textAlign={'center'}>Item</Table.HeaderCell>
-                <Table.HeaderCell textAlign={'center'}>Quantity</Table.HeaderCell>
-                <Table.HeaderCell textAlign={'center'}>Status</Table.HeaderCell>
-                <Table.HeaderCell textAlign={'center'}>Truck Plate #</Table.HeaderCell>
-                <Table.HeaderCell textAlign={'center'}>Producer</Table.HeaderCell>
-                <Table.HeaderCell textAlign={'center'}>Company</Table.HeaderCell>
-                <Table.HeaderCell textAlign={'center'}>Phone #</Table.HeaderCell>
-=======
-  let {inventories, filterInventories, filteredInventories} = props
+  let {inventories, filterInventories, filteredInventories, loading} = props
   inventories = filteredInventories || inventories || []
   return (
     <div style={styles.mainContainer}>
@@ -58,6 +33,13 @@ const InventoryTable = props => {
           click <Button circular icon='add' size='mini'/>on an item card
         </Message>
       </Segment>
+
+      { loading
+        ? <Segment vertical padded>
+            <Loader active />
+          </Segment>
+        : <Fragment>
+          {inventories && !inventories.length && <Message negative>No available inventory yet</Message>}
       <Table celled sortable>
         <Table.Header>
           <Table.Row>
@@ -81,21 +63,7 @@ const InventoryTable = props => {
                 <Table.Cell textAlign={'center'}>{_.capitalize(inventory.status.replace('_', ' '))}</Table.Cell>
                 <Table.Cell
                   textAlign={'center'}>{inventory.producer ? _.upperCase(inventory.producer) : 'N/A'}</Table.Cell>
->>>>>>> finalization
               </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              {inventories && inventories.map(inventory => {
-                return (
-                  <Table.Row key={inventory._id}>
-                    <Table.Cell textAlign={'left'}>{inventory.itemName}</Table.Cell>
-                    <Table.Cell textAlign={'center'}>{inventory.quantity > 0 ? inventory.quantity : 'N/A'} </Table.Cell>
-                    <Table.Cell textAlign={'center'}>{inventory.status || 'N/A'}</Table.Cell>
-                    <Table.Cell textAlign={'center'}>{inventory.truckPlateNumber || 'N/A'}</Table.Cell>
-                    <Table.Cell textAlign={'center'}>{inventory.producer || 'N/A'}</Table.Cell>
-                    <Table.Cell textAlign={'center'}>{inventory.company || 'N/A'}</Table.Cell>
-                    <Table.Cell textAlign={'center'}>{inventory.poNumber || 'N/A'}</Table.Cell>
-                  </Table.Row>
                 )
               })}
             </Table.Body>
