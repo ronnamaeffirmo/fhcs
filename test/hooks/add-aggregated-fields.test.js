cd4609741190a71cf589994e5d347773c709fbc0
unknown <angellou101999@gmail.com>
2018-12-15 00:21:51 +0800

    add test to return error FHCS-39 #time 10m

--- test/hooks/add-aggregated-fields.test.js
+++ test/hooks/add-aggregated-fields.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const feathers = require('@feathersjs/feathers');
 const addAggregatedFields = require('../../src/hooks/add-aggregated-fields');
+const should = require('chai').should()
 
 describe('\'addAggregatedFields\' hook', () => {
   let app;
@@ -21,7 +22,14 @@ describe('\'addAggregatedFields\' hook', () => {
 
   it('runs the hook', async () => {
     const result = await app.service('dummy').get('test');
-    
     assert.deepEqual(result, { id: 'test' });
   });
+  it('should return an error', async () => {
+    // assert.throws(result, expected)
+    try {
+      const result = await app.service('dummy').get();
+    } catch(err) {
+      assert.strictEqual(err.name, 'Error')
+    }
+  })
 });
