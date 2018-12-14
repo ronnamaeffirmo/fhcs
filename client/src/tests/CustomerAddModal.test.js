3ec97657bcafb969ecf2f502a97441836ce75220
unknown <angellou101999@gmail.com>
2018-12-15 05:08:33 +0800

    add test src/tests/CustomerAddModal.test.js FHCS-38 #time 20m

---
+++ client/src/tests/CustomerAddModal.test.js
@@ -0,0 +1,31 @@
+import App from '../components/AddCustomerModal'
+
+import React from 'react'
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { shallow, mount } from 'enzyme'
+import sinon from 'sinon'
+import 'react-testing-library/cleanup-after-each'
+import 'jest-dom/extend-expect'
+import { render } from 'react-testing-library'
+
+configure({ adapter: new Adapter() });
+
+describe('AddItemModalForm', () => {
+  it('renders witout crashing', () => {
+    shallow(<App/>)
+  })
+
+  
+  it('renders welcome message', () => {
+    const { getByText } = render(<App />);
+    expect(getByText('NEW CUSTOMER')).toBeInTheDocument()
+  });
+
+  it('allows us to set props', () => {
+    const wrapper = mount(<App bar="baz"/>)
+    expect(wrapper.props().bar).toEqual('baz')
+    wrapper.setProps({ bar: 'App' })
+    expect(wrapper.props().bar).toEqual('App')
+  })
+})
