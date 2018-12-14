92e7a8a59d75b0efda75560319281778063d8785
unknown <angellou101999@gmail.com>
2018-12-15 01:04:11 +0800

    fix test to test/hooks/check-role-usage.test.js FHCS-39 #time 10m

--- test/hooks/check-role-usage.test.js
+++ test/hooks/check-role-usage.test.js
@@ -1,27 +1,44 @@
 const assert = require('assert');
 const feathers = require('@feathersjs/feathers');
 const checkRoleUsage = require('../../src/hooks/check-role-usage');
+const sinon = require('sinon')
 
 describe('\'check role usage\' hook', () => {
-  let app;
+  const serviceStub = sinon.stub()
+  const findStub = sinon.stub()
+  const hookStub = sinon.stub()
+
+  const appStub = {
+    service: serviceStub
+  }
+
+  appStub.service.returns({
+    find: findStub,
+    hooks: hookStub,
+  })
 
   beforeEach(() => {
-    app = feathers();
+    findStub.returns({
+      id: '123'
+    })
 
-    app.use('/dummy', {
-      async get(id) {
-        return { id };
-      }
-    });
-
-    app.service('dummy').hooks({
+    hookStub.returns({
       before: checkRoleUsage()
-    });
+    })
+    appStub.service('dummy').hooks()
   });
 
   it('runs the hook', async () => {
-    const result = await app.service('dummy').get('test');
+    const result = await appStub.service('dummy').find('123');
     
-    assert.deepEqual(result, { id: 'test' });
+    assert.deepEqual(result, { id: '123' });
   });
+
+  it('should return an error when parameter is empty', async () => {
+    try {
+      const result = await app.service('dummy').find();
+    } catch(err) {
+      assert.strictEqual(err.name, 'ReferenceError')
+    }
+  })
 });
