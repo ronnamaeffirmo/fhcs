714f9dbc73a14e1fb360d2fb3bb467eb94d079ad
unknown <angellou101999@gmail.com>
2018-12-15 16:01:45 +0800

    add src/tests/AddRole.test.js FHCS-38 #time 20m

---
+++ client/src/tests/AddRole.test.js
@@ -0,0 +1,50 @@
+import App from '../components/AddRole'
+import RoleForm from '../components/RoleForm';
+
+import React from 'react'
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { shallow, mount } from 'enzyme'
+import sinon from 'sinon'
+import 'react-testing-library/cleanup-after-each'
+import 'jest-dom/extend-expect'
+import { render } from 'react-testing-library'
+import { reduxForm } from 'redux-form'
+
+configure({ adapter: new Adapter() });
+
+describe('AddItemModal', () => {
+  const testapp = reduxForm({
+    form: 'roleForm' 
+  })(App)
+
+  it('renders without crashing', () => {
+    shallow(<App/>)
+  });
+
+  it('renders RoleForm', () => {
+    const wrapper = shallow(<App/>)
+    expect(wrapper.find(RoleForm).length).toEqual(1)
+  });
+
+  it('renders welcome message', () => {
+    const { getByText } = render(<testapp />);
+    expect(getByText('')).toBeInTheDocument()
+  });
+
+  it('renders welcome message', () => {
+    const wrapper = shallow((
+      <testapp>
+        <div className="unique" />
+      </testapp>
+    ))
+    expect(wrapper.contains(<div className="unique" />)).toEqual(true)
+  });
+
+  it('allows us to set props', () => {
+    const wrapper = mount(<testapp bar="baz"/>)
+    expect(wrapper.props().bar).toEqual('baz')
+    wrapper.setProps({ bar: 'App' })
+    expect(wrapper.props().bar).toEqual('App')
+  });
+})
