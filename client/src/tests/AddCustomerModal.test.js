749fad393dfa0db2c75c1360289ebb3db78f4e96
unknown <angellou101999@gmail.com>
2019-01-04 12:12:52 +0800

    modify src/tests/AddCustomerModal.test.js FHCS-38 #time 15m

--- client/src/tests/AddCustomerModal.test.js
+++ client/src/tests/AddCustomerModal.test.js
@@ -1,29 +1,32 @@
 import App from '../components/AddCustomerModal'
+import CustomerForm from '../components/CustomerForm'
 
 import React from 'react'
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import { shallow, mount } from 'enzyme'
-import 'react-testing-library/cleanup-after-each'
-import 'jest-dom/extend-expect'
-import { render } from 'react-testing-library'
+import { shallow, mount, render } from 'enzyme'
 
-configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() })
 
 describe('AddCustomerModal', () => {
   it('renders witout crashing', () => {
-    shallow(<App/>)
-  })
-  
-  it('renders welcome message', () => {
-    const { getByText } = render(<App />);
-    expect(getByText('NEW CUSTOMER')).toBeInTheDocument()
+    shallow(<App />)
   })
 
+  it('renders <CustomerForm/> components', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find(CustomerForm)).toHaveLength(1)
+  })
+  
   it('allows us to set props', () => {
     const wrapper = mount(<App bar="baz"/>)
     expect(wrapper.props().bar).toEqual('baz')
     wrapper.setProps({ bar: 'App' })
     expect(wrapper.props().bar).toEqual('App')
+  })
+
+  it('renders the title NEW CUSTOMER', () => {
+    const wrapper = render(<App />)
+    expect(wrapper.text()).toContain("NEW CUSTOMER")
   })
 })
