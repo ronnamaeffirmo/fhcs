ab57c84b42637454b951919eb4f1f7bd75bb2d22
unknown <angellou101999@gmail.com>
2019-01-05 11:06:33 +0800

    add test CustomerForm.test.js FHCS-38 #time 10m

---
+++ client/src/tests/components/CustomerForm.test.js
@@ -0,0 +1,36 @@
+import App from '../../components/CustomerForm'
+
+import React from 'react'
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { shallow, mount, render } from 'enzyme'
+import { reduxForm } from 'redux-form'
+
+configure({ adapter: new Adapter() })
+
+describe('CustomerForm', () => {
+  const testapp = reduxForm({
+    form: 'customerForm' 
+  })(App)
+
+  it('renders witout crashing', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('renders children when passed in', () => {
+    const wrapper = shallow((
+      <testapp>
+        <div className="unique" />
+      </testapp>
+    ))
+    expect(wrapper.contains(<div className="unique" />)).toEqual(true)
+  })
+
+  it('allows us to set props', () => {
+    const wrapper = mount(<testapp bar="baz"/>)
+    expect(wrapper.props().bar).toEqual('baz')
+    wrapper.setProps({ bar: 'App' })
+    expect(wrapper.props().bar).toEqual('App')
+  })
+})
