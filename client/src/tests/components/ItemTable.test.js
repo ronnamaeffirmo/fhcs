1be3bf04f90480d89bd89b8e7f92ade126587d34
unknown <angellou101999@gmail.com>
2019-01-11 10:33:52 +0800

    add test ItemTable FHCS-38 #time 14m

---
+++ client/src/tests/components/ItemTable.test.js
@@ -0,0 +1,36 @@
+import App from '../../components/ItemTable'
+
+import React from 'react'
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import { shallow, mount, render } from 'enzyme'
+import sinon from 'sinon'
+
+configure({ adapter: new Adapter() })
+
+describe('ItemTable', () => {
+  const getItemsStub = sinon.stub()
+  it('renders without crashing', () => {
+    shallow(<App />)
+  })
+
+  it('renders 1 <div />', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('div')).toHaveLength(1)
+  })
+
+  it('renders 1 <Table />', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('Table')).toHaveLength(1)
+  })
+
+  it('renders 1 <Segment />', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('Segment')).toHaveLength(1)
+  })
+
+  it('renders 1 <TableHeader />', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('TableHeader')).toHaveLength(1)
+  })
+})
